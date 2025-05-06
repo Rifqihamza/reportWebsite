95e92b42a122a146ce1a2a7f90abdc703598e799
Iwansal64 <iwansal64@gmail.com>
2025-05-06 15:30:57 +0700

    Forgot to put initialization in some endpoints

--- src/pages/api/user/logout.ts
+++ src/pages/api/user/logout.ts
@@ -1,8 +1,10 @@
 import type { APIContext } from "astro";
-import { create_response_cookie } from "../../../utils/api_helper";
+import { create_response_cookie, first_initialization } from "../../../utils/api_helper";
 import cookie from 'cookie';
 
 export async function POST({}: APIContext) {
+    await first_initialization();
+
     const clear_user_token_cookie = cookie.serialize("user_token", "", {
         expires: new Date(),
         path: '/',
