c89ca13eaef6a91ae49200290d0efff621a4f789
Iwansal64 <iwansal64@gmail.com>
2025-05-10 18:15:17 +0700

    Logout Button Position changed to Fixed

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -3,7 +3,7 @@ import { AccountType, type User } from "../types/variables";
 export default function LogoutButton({ userData, handle_logout }: { userData: User, handle_logout: () => Promise<void> }) {
     return <div className="relative group md:block hidden">
     <button
-      className="flex items-center overflow-hidden px-4 py-3 bg-white hover:bg-[#7FA1C3] hover:text-white rounded-bl-xl absolute top-0 right-0 hover:w-[6rem] w-[3rem] duration-300 cursor-pointer group"
+      className="flex items-center overflow-hidden px-4 py-3 bg-white hover:bg-[#7FA1C3] hover:text-white rounded-bl-xl fixed top-0 right-0 hover:w-[6rem] w-[3rem] duration-300 cursor-pointer group"
       onClick={handle_logout}
     >
       <i className="pi pi-sign-out mr-2"></i>
