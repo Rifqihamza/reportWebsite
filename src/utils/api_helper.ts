3bcd7d88612db56919cbdb3c34cdfbcace05efa7
Iwansal64 <iwansal64@gmail.com>
2025-05-13 22:18:33 +0700

    Fix Upload Image Bug:
    PHP Server is not getting cookie from client side

--- src/utils/api_helper.ts
+++ src/utils/api_helper.ts
@@ -5,7 +5,6 @@ import { prisma } from "./db";
 import { AccountType } from "@prisma/client";
 
 let done_initialization = false;
-export const php_server_url = "http://localhost/web-report/upload_image.php";
 
 // First Initialization
 export async function first_initialization() {
