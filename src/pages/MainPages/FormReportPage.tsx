1c8e0047e9e1c61f33c01cff9cd73ecc1c33680e
Iwansal64 <81747549+iwansal64@users.noreply.github.com>
2025-05-15 22:00:15 +0700

    Fix performance issue regarding material UI icons

--- src/pages/MainPages/FormReportPage.tsx
+++ src/pages/MainPages/FormReportPage.tsx
@@ -1,14 +1,13 @@
 import type { Dispatch, SetStateAction } from "react";
 import ReportFormComponent from "../../components/FormReport/ReportFormComponent";
 import type { ReportData } from "../../types/variables";
-import ReportIcon from '@mui/icons-material/Report';
 
 export default function ReportForm({ reportData, setReportData }: { reportData: ReportData[], setReportData: Dispatch<SetStateAction<ReportData[]>> }) {
 
   return (
     <>
       <div className="flex flex-row gap-2 justify-center items-center mb-4 md:justify-normal">
-        <ReportIcon fontSize="medium" />
+        <i className="pi pi-exclamation-circle" />
         <h1 className="titlePage">Laporkan Temuanmu</h1>
       </div>
       {/* End Header Title */}
