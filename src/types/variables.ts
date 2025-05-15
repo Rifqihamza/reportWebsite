9f14811cc6b3209c5cf97fb06d24f295cf2f907d
cirengspicy <cirengspicy@gmail.com>
2025-05-15 13:06:37 +0700

    idk

--- src/types/variables.ts
+++ src/types/variables.ts
@@ -21,16 +21,16 @@ export function reporttype_to_string(data: any): string {
     return data == "VR" ? "5R" : data.toString()
 }
 
-export function string_to_reporttype(data: string): ReportType|undefined {
-    if(data == "5R") return ReportType.VR;
+export function string_to_reporttype(data: string): ReportType | undefined {
+    if (data == "5R") return ReportType.VR;
     return Object.values(ReportType).find(value => value.toString() == data);
 }
 
-export function string_to_accounttype(data: string): AccountType|undefined {
+export function string_to_accounttype(data: string): AccountType | undefined {
     return Object.values(AccountType).find(value => value.toString() == data);
 }
 
-export function string_to_reportstatus(data: string): ReportStatus|undefined {
+export function string_to_reportstatus(data: string): ReportStatus | undefined {
     return Object.values(ReportStatus).find(value => value.toString() == data);
 }
 
