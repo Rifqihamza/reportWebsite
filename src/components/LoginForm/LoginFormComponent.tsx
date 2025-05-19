2f775e9f9f6300dfc6d82816492d6b9f88dc5c47
Iwansal64 <81747549+iwansal64@users.noreply.github.com>
2025-05-19 10:31:31 +0700

    Fix some bugs

--- src/components/LoginForm/LoginFormComponent.tsx
+++ src/components/LoginForm/LoginFormComponent.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useState } from "react";
 import { APIResultType, userLogin } from "../../utils/api_interface";
 
@@ -6,65 +6,80 @@ export default function LoginFormComponent() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loginDisabled, setLoginDisabled] = useState(false);
-  
+
   const handleLogin = async () => {
     setLoginDisabled(true);
-    
-    const result = await userLogin(username, password);
-    if(result == APIResultType.NoError) {
-      
+
+    try {
+      const result = await userLogin(username, password);
+      if (result == APIResultType.NoError) {
         window.location.href = "/";
-    }
-    else if(result == APIResultType.Unauthorized) {
+      } else if (result == APIResultType.Unauthorized) {
         alert("Unauthorized!");
-    }
-    else {
+      } else {
         alert("There's an error!");
+      }
+    } catch (err) {
+      alert("There's an unexpected error..");
     }
-    
 
     setLoginDisabled(false);
-  }
+  };
 
-  return <>
-    {/* Username */}
-    <div className="space-y-2 mt-6">
-      <label htmlFor="username" className="font-bold"> Username </label>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username..."
-        autoComplete="off"
-        className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black"
-        onChange={(e) => setUsername(e.target.value)}
-        required
-      />
-    </div>
+  return (
+    <>
+      {/* Username */}
+      <div className="space-y-2 mt-6">
+        <label htmlFor="username" className="font-bold">
+          {" "}
+          Username{" "}
+        </label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username..."
+          autoComplete="off"
+          className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black"
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
 
-    {/* Password */}
-    <div className="space-y-2 mt-2">
-      <label htmlFor="password" className="font-bold"> Passsword </label>
-      <input
-        type="password"
-        name="password"
-        placeholder="Password..."
-        className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black"
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-    </div>
+      {/* Password */}
+      <div className="space-y-2 mt-2">
+        <label htmlFor="password" className="font-bold">
+          {" "}
+          Passsword{" "}
+        </label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password..."
+          className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
 
-    {/* Login Button */}
-    <div className="space-y-2 mt-8">
-      <button
-        type="button"
-        className="disabled:opacity-50 w-full uppercase tracking-[2px] font-bold px-6 py-2 bg-[#7FA1C3] -translate-y-[10px] [box-shadow:0_10px_0_#E2DAD6] active:[box-shadow:0_5px_0_#E2DAD6] active:-translate-y-[5px] text-white rounded-xl"
-        disabled={loginDisabled}
-        onClick={handleLogin}
-      >
-        {loginDisabled ? <i className="pi pi-spin pi-spinner" style={{ fontSize: '1rem', marginRight: '10px' }}></i> : ""}
-        Login
+      {/* Login Button */}
+      <div className="space-y-2 mt-8">
+        <button
+          type="button"
+          className="disabled:opacity-50 w-full uppercase tracking-[2px] font-bold px-6 py-2 bg-[#7FA1C3] -translate-y-[10px] [box-shadow:0_10px_0_#E2DAD6] active:[box-shadow:0_5px_0_#E2DAD6] active:-translate-y-[5px] text-white rounded-xl"
+          disabled={loginDisabled}
+          onClick={handleLogin}
+        >
+          {loginDisabled ? (
+            <i
+              className="pi pi-spin pi-spinner"
+              style={{ fontSize: "1rem", marginRight: "10px" }}
+            ></i>
+          ) : (
+            ""
+          )}
+          Login
         </button>
-    </div>
-  </>
+      </div>
+    </>
+  );
 }
