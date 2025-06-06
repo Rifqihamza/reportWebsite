0f3092411f10e4635245c24b33081cf972588b8c
Iwansal64 <iwansal64@gmail.com>
2025-06-06 21:06:42 +0700

    [tiny-patch](thanks) Remove stupid logs XD

--- src/components/ThanksOverlay/ThanksOverlayTrigger.tsx
+++ src/components/ThanksOverlay/ThanksOverlayTrigger.tsx
@@ -4,10 +4,5 @@ import { useThanksModalHook } from "../../hooks/shared/useThanksModal";
 export default function ThanksOverlayTrigger() {
     const { showThanks } = useThanksModalHook();
 
-    useEffect(() => {
-        console.log("Show Thanks:");
-        console.log(showThanks);
-    }, [showThanks]);
-
     return <span className={showThanks ? "show-thanks" : ""}></span>
 }
