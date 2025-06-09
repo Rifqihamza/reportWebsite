b38f465948ff781d143d26fea768b01fe896d97a
cirengspicy <cirengspicy@gmail.com>
2025-06-09 20:22:30 +0700

    someChanges in SelectCampus

--- src/pages/api/report_form/configuration.ts
+++ src/pages/api/report_form/configuration.ts
@@ -5,21 +5,21 @@ import { Campus, string_to_campus } from "../../../types/variables";
 
 export async function GET({ request }: APIContext) {
   const url = new URL(request.url);
-  
+
   const selected_campus_name: Campus | undefined = string_to_campus(url.searchParams.get("campus") ?? undefined);
 
-  if(!selected_campus_name) {
+  if (!selected_campus_name) {
     console.log(`url.searchParams.get("campus"): ${url.searchParams.get("campus")}`);
     return create_response_status(400);
   }
-  
+
   try {
     const parameter = {
       where: {
-        campus_name: selected_campus_name
+        campus_name: selected_campus_name 
       }
     };
-    
+
     const pic_data = await prisma.report_PIC.findMany(parameter);
     const location_data = await prisma.report_Location.findMany(parameter);
 
@@ -28,7 +28,7 @@ export async function GET({ request }: APIContext) {
       location_data: location_data
     });
   }
-  catch(err) {
+  catch (err) {
     console.log(`There's an error when trying to get report configuration data. Error: ${err}`);
     return create_response_status(500);
   }
