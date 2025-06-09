7ada34ab8e1ceeccefdf7dc03c5e9b87014c7aa2
Iwansal64 <iwansal64@gmail.com>
2025-06-09 21:15:14 +0700

    [fix](login) Fix the handler for wrong password input from user

--- src/components/LoginForm/LoginFormComponent.tsx
+++ src/components/LoginForm/LoginFormComponent.tsx
@@ -9,20 +9,16 @@ export default function LoginFormComponent() {
   const [password, setPassword] = useState("");
   const [loginDisabled, setLoginDisabled] = useState(false);
   const { showMessage } = useMessageToastHook();
-  
+
   const handleLogin = async () => {
     setLoginDisabled(true);
 
     try {
       const result = await userLogin(username, password);
-      if (typeof result === "string") {
-        if(result === AccountType.Guru) {
-          window.location.href = "/dashboard";
-        }
-        else {
-          window.location.href = "/";
-        }
-        return;
+      if (result === AccountType.Guru) {
+        window.location.href = "/dashboard";
+      } else if (Object.values(AccountType).find((val) => val.toString() == result.toString())) {
+        window.location.href = "/";
       } else if (result == APIResultType.Unauthorized) {
         showMessage("Unauthorized!", "error", "Wrong password or username");
       } else {
@@ -42,16 +38,7 @@ export default function LoginFormComponent() {
         <label htmlFor="username" className="font-bold">
           Username
         </label>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username..."
-          autoComplete="off"
-          className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black"
-          onChange={(e) => setUsername(e.target.value)}
-          onKeyDown={(e) => e.key == "Enter" ? handleLogin() : ""}
-          required
-        />
+        <input type="text" name="username" placeholder="Username..." autoComplete="off" className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black" onChange={(e) => setUsername(e.target.value)} onKeyDown={(e) => (e.key == "Enter" ? handleLogin() : "")} required />
       </div>
 
       {/* Password */}
@@ -59,33 +46,13 @@ export default function LoginFormComponent() {
         <label htmlFor="password" className="font-bold">
           Passsword
         </label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password..."
-          className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black"
-          onChange={(e) => setPassword(e.target.value)}
-          onKeyDown={(e) => e.key == "Enter" ? handleLogin() : ""}
-          required
-        />
+        <input type="password" name="password" placeholder="Password..." className="mt-3 bg-[#E2DAD6] shadow-inner shadow-gray-300 rounded-xl w-full px-4 py-3 outline-none focus:shadow-gray-400 focus:duration-300 focus:ease placeholder-black" onChange={(e) => setPassword(e.target.value)} onKeyDown={(e) => (e.key == "Enter" ? handleLogin() : "")} required />
       </div>
 
       {/* Login Button */}
       <div className="space-y-2 mt-8">
-        <button
-          type="button"
-          className="disabled:opacity-50 w-full uppercase tracking-[2px] font-bold px-6 py-2 bg-[#7FA1C3] -translate-y-[10px] [box-shadow:0_10px_0_#E2DAD6] active:[box-shadow:0_5px_0_#E2DAD6] active:-translate-y-[5px] text-white rounded-xl cursor-pointer"
-          disabled={loginDisabled}
-          onClick={handleLogin}
-        >
-          {loginDisabled ? (
-            <i
-              className="pi pi-spin pi-spinner"
-              style={{ fontSize: "1rem", marginRight: "10px" }}
-            ></i>
-          ) : (
-            ""
-          )}
+        <button type="button" className="disabled:opacity-50 w-full uppercase tracking-[2px] font-bold px-6 py-2 bg-[#7FA1C3] -translate-y-[10px] [box-shadow:0_10px_0_#E2DAD6] active:[box-shadow:0_5px_0_#E2DAD6] active:-translate-y-[5px] text-white rounded-xl cursor-pointer" disabled={loginDisabled} onClick={handleLogin}>
+          {loginDisabled ? <i className="pi pi-spin pi-spinner" style={{ fontSize: "1rem", marginRight: "10px" }}></i> : ""}
           Login
         </button>
       </div>
