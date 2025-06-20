500e089bce932308194714a9e6cf9f884b68a6ea
Iwansal64 <iwansal64@gmail.com>
2025-06-20 15:37:31 +0700

    [fix](reportTable) Fix some styling problems

--- src/components/DashboardComponents/TableDashboard/ReportMobileCard.tsx
+++ src/components/DashboardComponents/TableDashboard/ReportMobileCard.tsx
@@ -16,30 +16,30 @@ const ReportMobileCard: React.FC<ReportMobileCardProps> = ({ report }) => {
             data-report-id={report.id}
         >
             <div className="flex flex-col mb-2">
-                <h3 className="font-bold text-lg lg:text-2xl text-gray-900 truncate">Laporan terkait {reporttype_to_string(report.type)}</h3>
-                <p className="font-semibold text-xs lg:text-md truncate">{formatDate(new Date(report.created_at).toISOString())}</p>
+                <h3 className="text-lg lg:text-2xl text-gray-900 truncate">Laporan terkait <span className="font-semibold">{reporttype_to_string(report.type)}</span></h3>
+                <p className="text-xs lg:text-md truncate">{formatDate(new Date(report.created_at).toISOString())}</p>
             </div>
-            <div className="text-sm text-gray-500 space-y-2 break-all mt-4">
+            <div className="text-sm text-gray-500 space-y-2 break-all mt-4 *:font-medium">
                 <p>
-                    <span className="font-semibold">Nama Pelapor: </span> {report.submitted_by}
+                    <span className="font-bold">Nama Pelapor: </span> {report.submitted_by}
                 </p>
                 <p>
-                    <span className="font-semibold">Laporan: </span>{report.message}
+                    <span className="font-bold">Laporan: </span>{report.message}
                 </p>
                 <p>
-                    <span className="font-semibold">Lokasi:</span> {report.location_name}
+                    <span className="font-bold">Lokasi:</span> {report.location_name}
                 </p>
                 <p>
-                    <span className="font-semibold">PIC:</span> {report.pic_name || "Belum ditentukan"}
+                    <span className="font-bold">PIC:</span> {report.pic_name || "Belum ditentukan"}
                 </p>
                 <p>
-                    <span className="font-semibold">Kategori:</span> {reporttype_to_string(report.type)}
+                    <span className="font-bold">Kategori:</span> {reporttype_to_string(report.type)}
                 </p>
                 <p>
-                    <span className="font-semibold">Tanggal Temuan:</span> {formatDate(new Date(report.report_date).toISOString())}
+                    <span className="font-bold">Tanggal Temuan:</span> {formatDate(new Date(report.report_date).toISOString())}
                 </p>
                 <p>
-                    <span className="font-semibold">Due Date:</span> {report.due_date ? formatDate(new Date(report.due_date).toISOString()) : "Belum ditentukan"}
+                    <span className="font-bold">Due Date:</span> {report.due_date ? formatDate(new Date(report.due_date).toISOString()) : "Belum ditentukan"}
                 </p>
                 <p>
                     Status Laporan: <span className={`px-6 py-1 text-xs font-semibold rounded-full truncate ${statusColors[report.status]}`}> {report.status}</span>
