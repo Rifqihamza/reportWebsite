85bc074dce3d9419d2f3e0d593a8c65b1058322a
Iwansal64 <iwansal64@gmail.com>
2025-06-25 07:25:35 +0700

    [fix](NotFoundPage) Removes middleware

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { MiddlewareHandler } from "astro";
-
-export const onRequest: MiddlewareHandler = async (context, next) => {
-    if (context.url.pathname !== "/") {
-        return context.redirect('/', 307); // temporary
-    }
-
-    return next();
-};
