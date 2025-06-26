e3c22dddb73d0a0f0a44b2e60598b765e8689471
cirengspicy <cirengspicy@gmail.com>
2025-06-26 11:16:40 +0700

    modifed a button to login

--- src/components/AboutComponents/AboutNavbar/AboutNavbar.tsx
+++ src/components/AboutComponents/AboutNavbar/AboutNavbar.tsx
@@ -1,12 +1,10 @@
+import DirectButton from "../../GlobalComponents/DirectButton/DirectButton";
+
 export default function AboutNavbar() {
-  const handleRedirect = () => {
-    window.location.href = "/loginPage";
-  };
-  
   return <>
-    <nav className="sticky md:fixed top-0 w-dvw py-4  px-6 bg-white shadow shadow-gray-200 flex flex-row z-100 items-center justify-between gap-2">
+    <nav className="fixed top-0 w-full py-4 px-6 bg-white shadow shadow-gray-200 flex flex-row z-100 items-center justify-between gap-2">
       <a className="text-2xl font-thin tracking-wide cursor-pointer" href="#">E-LAPOR</a>
-      <button onClick={handleRedirect} className="rounded-lg py-2 px-12 bg-[#1f324d] text-white overflow-hidden cursor-pointer duration-250 hover:bg-white hover:shadow-lg hover:text-[#1f324d]">Login</button>
+      <DirectButton label="Login"/>
     </nav>
   </>;
 }
---
+++ src/components/GlobalComponents/DirectButton/DirectButton.tsx
@@ -0,0 +1,10 @@
+interface propsBtn {
+    label: string;
+}
+
+export default function DirectButton({ label }: propsBtn) {
+    const handleRedirect = () => {
+        window.location.href = "/loginPage"
+    }
+    return <button onClick={handleRedirect} className="cursor-pointer border px-4 py-2 rounded-xl hover:bg-[#1f324d] hover:text-white duration-300">{label}</button>
+}
