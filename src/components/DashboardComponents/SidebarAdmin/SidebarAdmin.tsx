a5634cbd1b78e6ffa29a16c46246a95ee63ced25
cirengspicy <cirengspicy@gmail.com>
2025-07-10 01:04:02 +0700

    change style of the welcome page and sidebar admin

--- src/components/DashboardComponents/SidebarAdmin/SidebarAdmin.tsx
+++ src/components/DashboardComponents/SidebarAdmin/SidebarAdmin.tsx
@@ -67,7 +67,7 @@ export default function SidebarDashboard() {
                             className={`relative w-full text-left group py-3 font-semibold uppercase tracking-wider space-x-3 transition-colors duration-300 ${activeTab === 1 ? "text-[#1f324d]" : "text-black/50"
                                 }`}
                         >
-                            <i className="pi pi-clipboard"></i>
+                            <i className="pi pi-table"></i>
                             Table Report
                             <span
                                 className={`absolute bottom-0 left-0 h-1 bg-[#1f324d] rounded-full transition-all duration-500 ${activeTab === 1 ? "w-full" : "w-0 group-hover:w-full group-hover:left-0"
