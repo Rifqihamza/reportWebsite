6a29a60d6aa25b90f7aa305fd79481ba9bcaded3
Iwansal64 <iwansal64@gmail.com>
2025-07-10 22:20:05 +0700

    [fix](DashboardPage) Tab in sidebar only visible if the user has access to it

--- src/components/DashboardComponents/SidebarAdmin/SidebarAdmin.tsx
+++ src/components/DashboardComponents/SidebarAdmin/SidebarAdmin.tsx
@@ -3,25 +3,27 @@ import { useUserDataHook } from "../../../hooks/shared/useUserData";
 import { userLogout } from "../../../utils/api_interface";
 import { useDashboardNavbarHook } from "../../../hooks/shared/useDashboardNavbar";
 import { useNetworkConnectivityHook } from "../../../hooks/shared/useNetworkConnectivity";
+import { AccountAPIPrivillage } from "../../../types/variables";
 
 interface MenuItem {
     id: number;
     label: string;
     icon: string;
+    privillage?: AccountAPIPrivillage
 }
 
 const menuItems: MenuItem[] = [
     { id: 0, label: "Home", icon: "pi pi-home" },
-    { id: 1, label: "Table", icon: "pi pi-table" },
-    { id: 2, label: "Statistics", icon: "pi pi-chart-bar" },
-    { id: 3, label: "Export", icon: "pi pi-file-export" },
-    { id: 4, label: "Users", icon: "pi pi-user" },
-    { id: 5, label: "Setting", icon: "pi pi-cog" },
+    { id: 1, label: "Table", icon: "pi pi-table", privillage: AccountAPIPrivillage.GetReport },
+    { id: 2, label: "Statistics", icon: "pi pi-chart-bar", privillage: AccountAPIPrivillage.StatisticsPage },
+    { id: 3, label: "Export", icon: "pi pi-file-export", privillage: AccountAPIPrivillage.ExportPage },
+    { id: 4, label: "Users", icon: "pi pi-user", privillage: AccountAPIPrivillage.UsersPage },
+    { id: 5, label: "Setting", icon: "pi pi-cog", privillage: AccountAPIPrivillage.SettingProfile },
 ];
 
 export default function SidebarDashboard() {
     const { showSidebar, setShowSidebar, activeTab, setActiveTab } = useDashboardNavbarHook();
-    const { userData } = useUserDataHook();
+    const { userData, userPrivillages } = useUserDataHook();
     const { isConnected } = useNetworkConnectivityHook();
 
     useEffect(() => {
@@ -41,22 +43,28 @@ export default function SidebarDashboard() {
         }
     };
 
-    const renderMenuItem = ({ id, label, icon }: MenuItem) => (
-        <li key={id}>
-            <button
-                onClick={() => setActiveTab(id)}
-                className={`relative w-full text-left group py-3 font-semibold uppercase tracking-wider space-x-3 transition-colors duration-300 ${activeTab === id ? "text-white" : "text-gray-300"
-                    }`}
-            >
-                <i className={icon}></i>
-                {label}
-                <span
-                    className={`absolute bottom-0 left-0 h-1 bg-white rounded-full transition-all duration-500 ${activeTab === id ? "w-full" : "w-0 group-hover:w-full group-hover:left-0"
+    const renderMenuItem = ({ id, label, icon, privillage }: MenuItem) => {
+        if(privillage && !userPrivillages.includes(privillage)) {
+            return <></>;
+        }
+        
+        return (
+            <li key={id}>
+                <button
+                    onClick={() => setActiveTab(id)}
+                    className={`relative w-full text-left group py-3 font-semibold uppercase tracking-wider space-x-3 transition-colors duration-300 ${activeTab === id ? "text-white" : "text-gray-300"
                         }`}
-                ></span>
-            </button>
-        </li>
-    );
+                >
+                    <i className={icon}></i>
+                    {label}
+                    <span
+                        className={`absolute bottom-0 left-0 h-1 bg-white rounded-full transition-all duration-500 ${activeTab === id ? "w-full" : "w-0 group-hover:w-full group-hover:left-0"
+                            }`}
+                    ></span>
+                </button>
+            </li>
+        );
+    };
 
     return (
         <div
