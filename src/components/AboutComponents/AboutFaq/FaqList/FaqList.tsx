e4d6715e5d3b751721be70b70c4e0e1ca415d3a6
cirengspicy <cirengspicy@gmail.com>
2025-07-14 18:39:22 +0700

    re design About Page

--- src/components/AboutComponents/AboutFaq/FaqList/FaqList.tsx
+++ src/components/AboutComponents/AboutFaq/FaqList/FaqList.tsx
@@ -4,12 +4,12 @@ import { list_of_faq } from "../../../../types/faq";
 export default function FaqList() {
 
   return <>
-    <Accordion className="bg-white! **:bg-white! w-full">
-        {Object.keys(list_of_faq).map((question, index) => {
-          return <AccordionTab key={index} header={question} className="text-sm md:text-md">
-            <p>{list_of_faq[question]}</p>
-          </AccordionTab>;
-        })}
+    <Accordion className=" **:text-white! **:rounded-2xl! [&_.p-accordion-header]:mt-2 [&_.p-accordion-header]:bg-transparent! [&_.p-accordion-header-link]:bg-[#257180]! [&_.p-accordion-content]:bg-[#CB6040]! [&_.p-accordion-content]:mt-2! w-full">
+      {Object.keys(list_of_faq).map((question, index) => {
+        return <AccordionTab key={index} header={question} className="text-sm md:text-md">
+          <p>{list_of_faq[question]}</p>
+        </AccordionTab>;
+      })}
     </Accordion>
   </>;
 }
--- src/components/GlobalComponents/DirectButton/DirectButton.tsx
+++ src/components/GlobalComponents/DirectButton/DirectButton.tsx
@@ -6,5 +6,5 @@ export default function DirectButton({ label }: propsBtn) {
     const handleRedirect = () => {
         window.location.href = "/loginPage"
     }
-    return <button onClick={handleRedirect} className="bg-[#253149] border-[#253149] border-2 text-white px-4 py-2 rounded-2xl cursor-pointer duration-200 hover:bg-transparent hover:text-[#253149] hover:px-8">{label}</button>
+    return <button onClick={handleRedirect} className="bg-transparent border-[#F2E5BF] border-2 text-white px-4 py-2 rounded-2xl cursor-pointer duration-200 hover:bg-[#F2E5BF] hover:text-[#257180]">{label}</button>
 }
