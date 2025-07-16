0f1d081309a35de00c1207cd1ceb8df17c555b3c
cirengspicy <cirengspicy@gmail.com>
2025-07-16 12:07:35 +0700

    re-style aboutpage

--- src/components/AboutComponents/AboutNavbar/AboutNavbar.tsx
+++ src/components/AboutComponents/AboutNavbar/AboutNavbar.tsx
@@ -15,8 +15,8 @@ export default function AboutNavbar() {
   return (
     <>
       <nav
-        className={`w-full flex flex-row z-50 items-center justify-between px-6 py-3 text-white
-        ${isScrolled ? 'sticky top-0 bg-[#257180] shadow shadow-gray-300 translate-0 py-5 duration-500' : 'relative bg-[#257180] duration-500'}`}
+        className={`w-full flex flex-row z-50 items-center justify-between px-6 py-4 text-white
+        ${isScrolled ? 'fixed top-0 bg-[#257180] shadow-sm shadow-gray-800 translate-0 py-5 duration-500' : 'relative bg-[#257180] duration-500'}`}
       >
         <a className="text-2xl font-normal tracking-wide cursor-pointer" href="#">E-LAPOR</a>
         <button onClick={() => { window.location.href = "/loginPage" }} className="cursor-pointer font-normal text-md uppercase tracking-wider bg-[#CB6040] hover:bg-[#FD8B51] hover:text-[#F2E5BF] font-normal py-1 px-4 rounded-lg duration-300 transition-colors">Lapor</button>
