97207e16f7a44d033ed56edec100177ddcb2564e
cirengspicy <cirengspicy@gmail.com>
2025-07-16 12:26:32 +0700

    change color on AboutFAQ

--- src/components/AboutComponents/AboutFaq/FaqList/FaqList.tsx
+++ src/components/AboutComponents/AboutFaq/FaqList/FaqList.tsx
@@ -4,7 +4,7 @@ import { list_of_faq } from "../../../../types/faq";
 export default function FaqList() {
 
   return <>
-    <Accordion className=" **:text-white! **:rounded-xl! [&_.p-accordion-header]:mt-2 [&_.p-accordion-header]:bg-transparent! [&_.p-accordion-header-link]:border-none! [&_.p-accordion-header-link]:bg-[#257180]! [&_.p-accordion-content]:bg-[#FD8B51]! [&_.p-accordion-content]:border-none! [&_.p-accordion-content]:mt-2! w-full">
+    <Accordion className=" **:rounded-xl! [&_.p-accordion-header]:mt-2 [&_.p-accordion-header]:bg-transparent! [&_.p-accordion-header-link]:border-none! [&_.p-accordion-header-link]:bg-[#257180]! [&_.p-accordion-header-link]:text-white! [&_.p-accordion-content]:bg-[#F2E5BF]! [&_.p-accordion-content]:text-black! [&_.p-accordion-content]:border-none! [&_.p-accordion-content]:mt-2! w-full">
       {Object.keys(list_of_faq).map((question, index) => {
         return <AccordionTab key={index} header={question} className="text-sm md:text-md">
           <p>{list_of_faq[question]}</p>
