3b4d53ef62c05fe1be4c86505c1d116d6d78962d
Iwansal64 <iwansal64@gmail.com>
2025-07-17 04:39:33 +0700

    [adjust](ReportAPI) Adjust security measurements

--- src/pages/api/report/add.ts
+++ src/pages/api/report/add.ts
@@ -121,8 +121,7 @@ export async function POST({ request, cookies }: APIContext) {
         const form_data = new FormData();
         form_data.append("image", image);
 
-        const report_num = (await prisma.report.findMany()).length;
-        const server_token = process_server_token(report_num);
+        const server_token = process_server_token();
 
 
         let response;
