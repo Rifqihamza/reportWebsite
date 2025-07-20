45bf0504e55892e607d73518a05b38faafda7147
Coders <rifqihamza30@gmail.com>
2025-07-20 17:18:34 +0700

    change color and animation for ThanksOverlay

--- src/components/GlobalComponents/ThanksOverlay/ThanksCloseButton.tsx
+++ src/components/GlobalComponents/ThanksOverlay/ThanksCloseButton.tsx
@@ -6,6 +6,6 @@ export default function ThanksCloseButton() {
   const handleClose = () => {
     setShowThanks(false);
   }
-  
-  return <button className="px-4 py-4 bg-[#7FA1C3] text-white rounded-xl uppercase tracking-wider font-bold cursor-pointer" onClick={handleClose}>Selesai</button>;
+
+  return <button className="px-4 py-4 bg-[#CB6040] text-white rounded-xl uppercase tracking-wider font-bold cursor-pointer" onClick={handleClose}>Selesai</button>;
 }
