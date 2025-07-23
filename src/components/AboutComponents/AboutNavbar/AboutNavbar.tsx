fd310f5c90670625673ed5148edd479cd49cf563
Coders <rifqihamza30@gmail.com>
2025-07-23 11:39:22 +0700

    change shadow and blur navbar

--- src/components/AboutComponents/AboutNavbar/AboutNavbar.tsx
+++ src/components/AboutComponents/AboutNavbar/AboutNavbar.tsx
@@ -16,7 +16,7 @@ export default function AboutNavbar() {
     <>
       <nav
         className={`w-full flex flex-row z-50 items-center justify-between px-6 py-4 text-[#F2E5BF]
-        ${isScrolled ? 'fixed top-0 bg-white/5 backdrop-blur-3xl shadow shadow-gray-200 translate-0 py-5 duration-500' : 'fixed bg-transparent duration-200'}`}
+        ${isScrolled ? 'fixed top-0 bg-white/5 backdrop-blur-xs shadow shadow-black translate-0 py-5 duration-500' : 'fixed bg-transparent duration-200'}`}
       >
         <a className="text-2xl font-normal tracking-wide cursor-pointer" href="#">E-LAPOR</a>
         <button onClick={() => { window.location.href = "/loginPage" }} className="cursor-pointer font-normal text-md uppercase tracking-wider hover:shadow hover:shadow-white font-normal py-1 px-4 rounded-lg duration-300">Lapor</button>
