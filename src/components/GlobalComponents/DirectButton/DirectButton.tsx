ad595e401fca4346e9183f79427d8c53c15417ee
Coders <rifqihamza30@gmail.com>
2025-07-23 13:36:12 +0700

    adjustment color on AboutComponents

--- src/components/GlobalComponents/DirectButton/DirectButton.tsx
+++ src/components/GlobalComponents/DirectButton/DirectButton.tsx
@@ -6,5 +6,5 @@ export default function DirectButton({ label }: propsBtn) {
     const handleRedirect = () => {
         window.location.href = "/loginPage"
     }
-    return <button onClick={handleRedirect} className="bg-transparent border-[#F2E5BF] border-2 text-white px-4 py-2 rounded-2xl cursor-pointer duration-200 hover:bg-[#F2E5BF] hover:text-[#257180]">{label}</button>
+    return <button onClick={handleRedirect} className="bg-transparent border-white border-2 text-white px-4 py-2 rounded-2xl cursor-pointer duration-300 hover:[box-shadow:0_0_10px_2px_#DCD7C9]">{label}</button>
 }
