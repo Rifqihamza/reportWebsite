9048ef291223763af884666335d7e6e35b23158d
Iwansal64 <iwansal64@gmail.com>
2025-08-02 11:02:50 +0700

    [improve](ReportTable) Increase mobile responsiveness and add clear button for each filter

--- src/components/DashboardComponents/TableDashboard/Toolbar/SelectFilterComponent.tsx
+++ src/components/DashboardComponents/TableDashboard/Toolbar/SelectFilterComponent.tsx
@@ -33,7 +33,7 @@ export default function FilterSelect() {
             </div>
         </span>
         <div className={`fixed right-0 top-0 bg-black/40 w-full h-full z-100 duration-1000 ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`} onClick={(e) => e.currentTarget == e.target ? setIsOpen(false) : ""}>
-            <div className={`flex flex-col items-center absolute right-0 top-0 bg-[#1f324d] w-[500px] h-full p-4 border-1 border-white rounded-l-xl duration-750 ${isOpen ? "translate-x-0 delay-500" : "translate-x-full delay-0"}`}>
+            <div className={`flex flex-col items-center absolute right-0 top-0 bg-[#1f324d] w-full md:w-[500px] h-full p-4 border-1 border-white md:rounded-l-xl duration-750 ${isOpen ? "translate-x-0 delay-500" : "translate-x-full delay-0"}`}>
                 <div className="w-full h-full flex flex-col items-center gap-2">
                     <h1 className="text-white font-thin text-4xl border-b-1 border-white p-2 text-center w-4/5">Filter Table</h1>
                     <br />
@@ -43,6 +43,7 @@ export default function FilterSelect() {
                     <FilterItem filter={true} label="Lokasi" placeholder="Semua Lokasi" options={Object.entries(locationOptions).map(([campus, locations]) => ({ label: campus, items: locations.map((location) => ({ label: location, value: [campus, location] }))}))} optionGroupLabel="label" optionGroupChildren="items" currentValue={currentLocationFilter} onChange={(newValue) => setCurrentLocationFilter(newValue)} />
                 </div>
                 <button className="h-fit p-4 w-full border-white border-1 text-white cursor-pointer duration-300 hover:bg-white hover:text-[#1f324d]" onClick={applyFilter}>Apply</button>
+                <button className="block md:hidden h-fit p-4 w-full border-white border-1 text-white cursor-pointer duration-300 hover:bg-white hover:text-[#1f324d] mt-2" onClick={() => setIsOpen(false)}>Close</button>
             </div>
         </div>
     </>
@@ -62,8 +63,8 @@ interface FilterItemProps {
 
 function FilterItem(props: FilterItemProps) {
 
-    return <div className="flex flex-row justify-between items-center w-4/5">
-        <h1 className="text-xl text-white">{props.label}</h1>
-        <MultiSelect filter={props.filter} display="chip" maxSelectedLabels={2} placeholder={props.placeholder} panelClassName="bg-[#1f324d]! [&_.p-multiselect-item]:hover:bg-white/40! [&_.p-multiselect-header]:bg-transparent! [&_.p-focus]:bg-transparent! **:text-white! [&_.p-highlight]:bg-white/20! [&_.p-multiselect-item-group]:bg-transparent! [&_.p-multiselect-filter]:bg-transparent! [&_.p-multiselect-filter]:border-white [&_.p-multiselect-filter]:border-1!" panelHeaderTemplate={!props.filter ? <></> : null} showSelectAll={false} className="bg-transparent! w-1/2 text-white! p-1 [&_.p-multiselect-token]:bg-white!" value={props.currentValue} onChange={(e) => props.onChange(e.value)} options={props.options} optionGroupLabel={props.optionGroupLabel} optionGroupChildren={props.optionGroupChildren} />
+    return <div className="flex flex-col md:flex-row justify-between items-center w-4/5">
+        <h1 className="text-xl text-white text-left w-full md:w-fit">{props.label}</h1>
+        <MultiSelect filter={props.filter} display="chip" maxSelectedLabels={2} placeholder={props.placeholder} panelClassName="bg-[#1f324d]! [&_.p-multiselect-item]:hover:bg-white/40! [&_.p-multiselect-header]:bg-transparent! [&_.p-focus]:bg-transparent! **:text-white! [&_.p-highlight]:bg-white/20! [&_.p-multiselect-item-group]:bg-transparent! [&_.p-multiselect-filter]:bg-transparent! [&_.p-multiselect-filter]:border-white [&_.p-multiselect-filter]:border-1!" panelHeaderTemplate={!props.filter ? <></> : null} showSelectAll={false} showClear className="bg-transparent! w-full md:w-3/4 text-white! p-1 [&_.p-multiselect-token]:bg-white!" value={props.currentValue} onChange={(e) => props.onChange(e.value)} options={props.options} optionGroupLabel={props.optionGroupLabel} optionGroupChildren={props.optionGroupChildren} />
     </div>;
 }
