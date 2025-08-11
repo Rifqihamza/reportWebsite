087673e0151e6c2f02482d13d9cf8dde7bba2f90
Iwansal64 <iwansal64@gmail.com>
2025-08-11 21:15:14 +0700

    [improve](AboutPage) Improve the interactivity in about page

--- src/components/AboutComponents/AboutFaq/FaqList/FaqList.tsx
+++ src/components/AboutComponents/AboutFaq/FaqList/FaqList.tsx
@@ -4,7 +4,7 @@ import { list_of_faq } from "../../../../types/faq";
 export default function FaqList() {
 
   return <>
-    <Accordion className=" **:text-white! **:rounded-xl! [&_.p-accordion-header]:mt-2 [&_.p-accordion-header]:bg-transparent! [&_.p-accordion-header-link]:border-none! [&_.p-accordion-header-link]:bg-[#2B3440]! [&_.p-accordion-content]:bg-[#F97316]! [&_.p-accordion-content]:border-none! [&_.p-accordion-content]:mt-2! w-full">
+    <Accordion className=" **:text-white! **:rounded-xl! [&_.p-accordion-header]:mt-2 [&_.p-accordion-header]:bg-transparent! [&_.p-accordion-header-link]:border-none! [&_.p-accordion-header-link]:bg-[#1A1D24]! [&_.p-accordion-content]:bg-[#1A1D24]! [&_.p-accordion-content]:border-none! [&_.p-accordion-content]:mt-2! w-full">
       {Object.keys(list_of_faq).map((question, index) => {
         return <AccordionTab key={index} header={question} className="text-sm md:text-md">
           <p>{list_of_faq[question]}</p>
