8e4e69260e00fbdfb2a5a4bf5a1c6ca10ab39a98
Iwansal64 <iwansal64@gmail.com>
2025-11-25 13:24:50 +0700

    [improve] Better line structure for middleware code

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,34 +10,34 @@ const rateLimiterMemory = new RateLimiterMemory({
 export const onRequest: MiddlewareHandler = async (context, next) => {
     await first_initialization();
     console.log(context.url.href);
-    
-    if(context.url.href.includes("api/") && !(await check_database_connection())) {
+
+    if (context.url.href.includes("api/") && !(await check_database_connection())) {
         return create_response_status(503);
     }
 
-    if(context.url.href.includes("api/") && (!context.url.href.includes("/captcha") && !context.url.href.includes("/login"))) {
+    if (context.url.href.includes("api/") && (!context.url.href.includes("/captcha") && !context.url.href.includes("/login"))) {
         // Now, Let's verify the user is already log in or not.
         const user_login_token = context.cookies.get("user_token")?.value;
-        if(!user_login_token) {
-            return create_response_status(401);
-        }
-        
-        const [verification_result, verification_output, user_data] = await verify_user_data_token(user_login_token);
-        if(!verification_result || user_data.inactive) {
+        if (!user_login_token) {
             return create_response_status(401);
         }
 
-        
+        const [verification_result, verification_output, user_data] = await verify_user_data_token(user_login_token);
+        if (!verification_result || user_data.inactive) {
+            return create_response_status(401);
+        }
+
+
         // Use ratelimiter to the user id
         try {
             await rateLimiterMemory.consume(user_data.id);
-            const res = await rateLimiterMemory.get(user_data.id)
+            const res = await rateLimiterMemory.get(user_data.id);
         } catch {
             return create_response_status(429); // stop here
         }
     }
-    
-    
+
+
     // Add security layer for response headers
     const response = await next();
 
